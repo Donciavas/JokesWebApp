59c55f0b53daec24ff6a1bcb395fe2bdaec1846c
Donatas Vitkus <vitk.donatas@gmail.com>
2022-11-26 15:17:37 +0200

    Adding .NET core auto generated project for a study purpose

---
+++ JokesWebApp/Migrations/20221007153022_Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Owner",
+                table: "Jokes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Owner",
+                table: "Jokes");
+        }
+    }
+}
